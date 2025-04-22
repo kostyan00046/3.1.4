092da258e9d293908c294593ad45461d74e07693
Konstantin Zarichnyuk <zarichnyuk95@bk.ru>
2025-04-22 14:16:00 +0300

    Go6

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +13,9 @@ public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
 
-    @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
-
 
     public UserDetails loadUserByUsername(String username) {
         return userDao.loadUserByUsername(username);
@@ -51,5 +48,4 @@ public class UserServiceImpl implements UserService {
     public void updateUser(int id, User updatedUser) {
         userDao.updateUser(id, updatedUser);
     }
-
 }
